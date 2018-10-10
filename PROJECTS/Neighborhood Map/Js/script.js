08c86a3fdff5f7d42a7789d3bc12b2118245c747
B.Vamsi Krishna <bommisettyvamsikrishna04@gmail.com>
2018-10-11 00:54:09 +0530

    Update script.js

--- PROJECTS/Neighborhood Map/Js/script.js
+++ PROJECTS/Neighborhood Map/Js/script.js
@@ -160,7 +160,7 @@ function populateInfoWindow(marker, infowindow){
 // of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
           '|40|_|%E2%80%A2',
           new google.maps.Size(21, 34),
           new google.maps.Point(0, 0),
@@ -192,7 +192,7 @@ function currentTemperature (lat,lng){
   console.log("Inside Current Temp Func");
   var current_temp = "";
   var secretKey = "2a8ad86741e4686531e403d321ed6a1f";
-  var darkskyAPI="https:api.darksky.net/forecast/"+secretKey+"/"+lat+","+lng+"?exclude=minutely,hourly,daily,alerts,flags&units=si"
+  var darkskyAPI="https://api.darksky.net/forecast/"+secretKey+"/"+lat+","+lng+"?exclude=minutely,hourly,daily,alerts,flags&units=si"
   console.log(darkskyAPI);
   $.ajax( darkskyAPI, {
     dataType: 'jsonp'
